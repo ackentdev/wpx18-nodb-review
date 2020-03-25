92a9c3aac756e167aa6b4b67a00e9f0e5bb1230e
ackentdev <adamkentdev@gmail.com>
2020-03-25 13:52:50 -0700

    added addTOdo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import List from './components/List';
+import AddToDo from './components/AddToDo';
 
 import './App.css';
 
@@ -8,9 +9,12 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      todos: []
+      todos: [],
+      newTodo: ''
     }
-    this.deleteTodo = this.deleteTodo.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.addToDo = this.addToDo.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
   }
   
   componentDidMount(){
@@ -22,16 +26,34 @@ class App extends React.Component {
   }
 
   deleteTodo(id){
-    axios.delete(`/api/delete_todo/${id}`).then(res => {
+    axios.delete(`/api/delete_todo/?id=${id}`).then(res => {
       this.setState({
         todos: res.data
       })
     })
 }
 
+  addToDo(){
+    axios.post('/api/add_todo', {todo: this.state.newTodo}).then(res => {
+      this.setState({
+        todos: res.data,
+        newTodo: ''
+      })
+    })
+  }
+
+  changeHandler(event){
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
+
+
   render(){
     return (
       <div className="App">
+        <AddToDo newTodo={this.state.newTodo} changeHandler={this.changeHandler} addToDo={this.addToDo}/>
         <List deleteTodo={this.deleteTodo} todos={this.state.todos}/>
       </div>
     );
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
 export default class AddToDo extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            newTodo: ''
-        }
-    }
 
     render(){
         return(
             <div>
-
+                <input
+                type="text"
+                name="newTodo"
+                value={this.props.newTodo}
+                onChange={(e) => this.props.changeHandler(e)}
+                placeholder="Add new task..."
+                />
+                <button onClick={this.props.addToDo}>Add ToDo</button>
             </div>
         )
     }
